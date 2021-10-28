e1abc1f426458bfa067b674f5f80cf4465c4a96b
Brennan Day <brenmarie2001@gmail.com>
2021-10-28 01:22:38 -0400

    Add HTML, Images, CSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
+var button = document.querySelector("button");
+var ul = document.querySelector("ul");
 
+function toggle() {
+  ul.classList.toggle("active");
+}
+
+button.addEventListener("click", toggle);
